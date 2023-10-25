16a38675d8fa6cccec7b38188adaa9191a58cb26
Lim200 <112889518+Lim200@users.noreply.github.com>
2023-10-25 19:17:42 +0300

    feat: done all strings task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -301,8 +301,18 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  let result = 0;
+  const arr = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (value === arr[i]) {
+      result = i;
+    }
+  }
+  return result;
 }
 
 
